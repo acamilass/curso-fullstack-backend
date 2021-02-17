d72dbaa049dcc693d321d1d735ffd1debde3e8df
Ana Camila Santos Sousa <ana.c.santos.sousa@avanade.com>
2021-02-17 19:44:00 -0300

    Criando a primeira requisição REST

--- src/main/java/clientes/model/entity/Cliente.java
+++ src/main/java/clientes/model/entity/Cliente.java
@@ -1,12 +1,19 @@
 package clientes.model.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.time.LocalDate;
 
 @Entity
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Cliente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -18,7 +25,12 @@ public class Cliente {
     @Column(nullable = false, length = 11)
     private String cpf;
 
-    @Column
-    @JoinColumn(name = "data_cadastro")
+    @Column(name = "data_cadastro")
+    @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate dataCadastro;
+
+    @PrePersist
+    public void prePersist() {
+        setDataCadastro(LocalDate.now());
+    }
 }
